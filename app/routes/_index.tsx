4f3864bb2620117b4220406d52e8596b20be05fc
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2025-02-09 16:06:40 +0900

    deployed to cloudflare workers with prerender support

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router";
+
+export async function loader() {
+	console.log("hello from loader");
+	return null;
+}
+
 export function meta() {
 	return [
 		{ title: "New React Router App" },
@@ -6,5 +13,9 @@ export function meta() {
 }
 
 export default function Home() {
-	return <>hello</>;
+	return (
+		<Link to="prerendered" className="font-bold text-destructive">
+			Go to prerendered page
+		</Link>
+	);
 }
