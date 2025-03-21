f837e65d56ac1b2f34909adbdab412eacd9670d7
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2025-03-21 21:33:16 +0900

    add a minimal dark mode support

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { ThemeProvider, useTheme } from "./context";
+import { cn } from "./utils/css";
 
 export const links: Route.LinksFunction = () => [
 	{ rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -32,8 +34,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<Meta />
 				<Links />
 			</head>
-			<body>
-				{children}
+			<body className={cn("w-full h-screen")}>
+				<ThemeProvider>{children}</ThemeProvider>
 				<ScrollRestoration />
 				<Scripts />
 			</body>
@@ -42,7 +44,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-	return <Outlet />;
+	const { theme } = useTheme();
+	return (
+		<div className={cn("w-full h-full bg-background text-foreground", theme === "dark" && "dark")}>
+			<Outlet />
+		</div>
+	);
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
+import { MoonIcon, SunIcon } from "lucide-react";
 import { Link } from "react-router";
+import { Switch } from "~/components/atoms/switch";
+import { useTheme } from "~/context";
 
 export async function loader() {
 	console.log("hello from loader");
@@ -13,9 +16,23 @@ export function meta() {
 }
 
 export default function Home() {
+	const { theme, setTheme } = useTheme();
 	return (
-		<Link to="prerendered" className="font-bold text-destructive">
-			Go to prerendered page
-		</Link>
+		<>
+			<div className="flex items-center space-x-2">
+				<SunIcon size={16} />
+				<Switch
+					id="airplane-mode"
+					checked={theme === "dark"}
+					onCheckedChange={(checked) => {
+						setTheme(checked ? "dark" : "light");
+					}}
+				/>
+				<MoonIcon size={16} />
+			</div>
+			<Link to="prerendered" className="font-bold text-destructive">
+				Go to prerendered page
+			</Link>
+		</>
 	);
 }
